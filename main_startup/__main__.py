399a229c13b81f5407abb128d06964103812ba2a
satya <82304386+timewithsatya@users.noreply.github.com>
2021-08-16 11:39:22 +0530

    Update __main__.py

--- main_startup/__main__.py
+++ main_startup/__main__.py
@@ -9,10 +9,6 @@
 import logging
 import os
 import platform
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from main_startup import Config, bot
-
-import pyrogram
 from pyrogram import __version__
 from bot_utils_files.Localization.engine import Engine
 from database.localdb import check_lang
@@ -129,27 +125,6 @@ You Can Visit @FridaySupportOfficial For Updates And @FridayChat For Any Query /
     logging.info(full_info)
     await pyrogram.idle()
 
-img = "https://telegra.ph//file/9384e275282985621e7c6.jpg"
-
-cap = "Friday Remastered v9.5"
-grp = Config.LOG_GRP
-
-bot.send_photo(
-     grp, 
-     img, 
-     caption=cap, 
-     reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "😎REPO😎",
-                            url="https://github.com/DevsExpo/FridayUserBot",
-                        )
-                    ],
-                  
-                ]
-            ),
-        ) 
 
 if __name__ == "__main__":
     Friday.loop.run_until_complete(run_bot())
